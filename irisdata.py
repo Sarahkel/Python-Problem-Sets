01642a9ea589cd83a9eac203550802626a21a72b
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-03-03 22:33:13 +0000

    Update irisdata.py

--- irisdata.py
+++ irisdata.py
@@ -6,6 +6,6 @@
 
 with open("data/iris.csv") as f:
     for line in f: 
-     print('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+        print('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
 
     
