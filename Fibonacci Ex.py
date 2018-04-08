8d6ba2140d1102da7877edf64141bbe5c09b1410
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-04-08 20:57:01 +0100

    Update Fibonacci Ex.py

--- Fibonacci Ex.py
+++ Fibonacci Ex.py
@@ -1,4 +1,4 @@
-# Sarah Scholz 10/02/2018
+# Sarah Scholz 10/02/2018 
 # Taken from: https://github.com/ianmcloughlin/python-fib/blob/master/fibname.py
 # A program that displays Fibonacci numbers using people's names.
 
@@ -14,11 +14,13 @@ def fib(n):
   
   return i
 
-name = "scholz"
+name = "Scholz"
+# my surname
 first = name[0]
 last = name[-1]
 firstno = ord(first)
 lastno = ord(last)
+# for definition of this function see below (line 47 onwards)
 x = firstno + lastno
 
 ans = fib(x)
@@ -31,9 +33,6 @@ print("Fibonacci number", x, "is", ans)
 # Forum Post EXERCISE 1
 # My name is Sarah, so the first and last letter of my name (S + H = 19 + 8) give the number  27. The 27th Fibonacci number is 196,418.
 
-
-
-
 # Forum Post EXERCISE 2
 #My surname is Scholz
 #
@@ -45,5 +44,6 @@ print("Fibonacci number", x, "is", ans)
 #
 #____
 #
-#ord()
-#returns the Unicode code point for a one-character string
+# ord() is a built-in function in Python. It is defined in the Python Standard Library: 
+# "Given a string representing one Unicode character, return an integer representing the Unicode code point of that character. For example, ord('a') returns the integer 97 and ord('€') (Euro sign) returns 8364. This is the inverse of chr()."
+# For reference see: https://docs.python.org/3/library/functions.html#ord
