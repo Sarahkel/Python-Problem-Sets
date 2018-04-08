d23d1f9b04ab36106944d6736773cd6a2ab4e6c0
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-04-08 21:02:51 +0100

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,5 +1,7 @@
 # Sarah Scholz 10/02/2018
 # Collatz Conjecture
+# Exercise: "single Python script that starts with an integer and repeatedly applies the Collatz function (divide by 2 if even, multiply by three and 1 if odd) using a while loop and if statement. At each iteration, the current value of the integer should be printed to the screen. "
+# More info on the conjecture:https://en.wikipedia.org/wiki/Collatz_conjecture
 
 n = int(input("Please enter a positive integer: "))
 print ("The Collatz Conjecture for the number", n, "works as follows:")
@@ -12,5 +14,3 @@ while (n != 1):
         n = n * 3 + 1
         print (n)
 print ("Yay, you arrived at", n)
-
-
