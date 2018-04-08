38263ce0d18b94e84660e13bf07e58459e389a53
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-04-08 22:04:34 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,11 @@
 # Sarah Scholz 04/03/2018
 # Function to find factorial of a given number
+# Factorial is the product of all positive integers less than or equal to n
 
 def factorial(x):
     f = 1
-    for i in range (1, x + 1):
-        f = i * f
+    for i in range (1, x + 1): # +1 to ensure the function counts inclusive x
+        f = i * f # f is multiplied repeatedly by all positive integers less than or equal to n
     return f
 
 print("The factorial of 5 is", factorial(5))
